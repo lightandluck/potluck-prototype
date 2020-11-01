3cd5a6dc9527d2401270c3263bd4bd1e6c314ec3
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2020-11-01 00:23:03 -0700

    Add styling to differentiate lists

--- client/src/components/exercises-list.component.js
+++ client/src/components/exercises-list.component.js
@@ -3,7 +3,19 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 const Exercise = props => (
-  <tr>
+  <tr className="offering">
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.exercise._id}>edit</Link> | <button onClick={() => { props.deleteExercise(props.exercise._id) }}>delete</button>
+    </td>
+  </tr>
+)
+
+const PotluckItem = props => (
+  <tr className="potluck-item">
     <td>{props.exercise.username}</td>
     <td>{props.exercise.description}</td>
     <td>{props.exercise.duration}</td>
@@ -62,7 +74,7 @@ export default class ExercisesList extends Component {
     })
   }
 
-  exerciseList() {
+  offeringsList() {
     let currentPlayer = this.state.playerName;
     return this.state.exercises
       .filter(el => el.username === currentPlayer)
@@ -71,12 +83,12 @@ export default class ExercisesList extends Component {
       })
   }
 
-  offeringsList () {
+  potluckList () {
     let currentPlayer = this.state.playerName;
     return this.state.exercises
       .filter(el => el.username !== currentPlayer)
       .map(currentexercise => {
-        return <Exercise exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
+        return <PotluckItem exercise={currentexercise} deleteExercise={this.deleteExercise} key={currentexercise._id}/>;
       })
   }
 
@@ -118,22 +130,8 @@ export default class ExercisesList extends Component {
             </tr>
           </thead>
           <tbody>
-            { this.exerciseList() }
-          </tbody>
-        </table>
-
-        <table className="table">
-          <thead className="thead-light">
-            <tr>
-              <th>Username</th>
-              <th>Description</th>
-              <th>Duration</th>
-              <th>Date</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
             { this.offeringsList() }
+            { this.potluckList() }
           </tbody>
         </table>
       </div>
