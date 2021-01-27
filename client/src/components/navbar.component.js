9b98881760746acbdc04eec086508abc42d85f36
Kevin Nguyen <kevin.nguyen@lightandluck.com>
2021-01-26 17:17:01 -0800

    Change NavBar into a functional component

--- client/src/components/navbar.component.js
+++ client/src/components/navbar.component.js
@@ -1,32 +1,37 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
-export default class Navbar extends Component {
-
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-        <Link to="/" className="navbar-brand">Potluck</Link>
-        <div className="navbar-collapse">
+// TODO: Rename all `xxxx.component.js` files to just `xxxx.js`. 
+// Also, capitalize their names because they are components (convention to capitalize)
+const NavBar = () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+      <Link to="/" className="navbar-brand">Potluck</Link>
+      <div className="navbar-collapse">
         <ul className="navbar-nav mr-auto">
           <li className="navbar-item">
-          <Link to="/" className="nav-link">Offerings</Link>
+            <Link to="/" className="nav-link">Offerings</Link>
           </li>
+
           <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Offering</Link>
+            <Link to="/create" className="nav-link">Create Offering</Link>
           </li>
+
           <li className="navbar-item">
-          <Link to="/player" className="nav-link">Create Player</Link>
+            <Link to="/player" className="nav-link">Create Player</Link>
           </li>
+
           <li className="navbar-item">
-          <Link to="/wishlist" className="nav-link">Wishlist</Link>
+            <Link to="/wishlist" className="nav-link">Wishlist</Link>
           </li>
+          
           <li className="navbar-item">
             <Link to="/totalwantlist" className="nav-link">Total Wantlist</Link>
           </li>
         </ul>
-        </div>
-      </nav>
-    );
-  }
+      </div>
+    </nav>
+  );
 }
+
+export default NavBar
